f86e5709684b3c3d06bdbcb2cdc01178fa67262f
Lucas <lcordoba844@gmail.com>
2024-05-27 15:00:33 -0300

    same but changed code to check b)

--- practica4/ejercicio3Func.php
+++ practica4/ejercicio3Func.php
@@ -7,9 +7,11 @@
     </head>
     <body>
         <?php
-            $fun = getdate();
-            echo "Has entrado en esta pagina a las $fun[hours] horas, con $fun[minutes] minutos y $fun[seconds]
-            segundos, del $fun[mday]/$fun[mon]/$fun[year]";
+            function sumar($sumando1,$sumando2){
+            $suma=$sumando1+$sumando2;
+            echo $sumando1."+".$sumando2."=".$suma;
+            }
+            sumar(5,6);
         ?>
     </body>
 </html>
